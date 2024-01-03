3d1556d545fa0641d700e8da792fe3b862860e7d
Pankaj Kumar <pankajpanwar3012@gmail.com>
2024-01-03 11:18:26 +0530

    func:AI Writing Assistant

---
+++ 3AI-Writing-Assistant/pages/index.js
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+
+
+const AIWritingAssistant = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  return (
+    <div className="root">
+      <div className='main'>
+        <div className='gradient'>
+        <Head>
+        <title>DeepWrite AI</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Article Generator</h1>
+          </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="Enter the title of the Article" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+            {/* New code I added here */}
+          <div className="prompt-buttons">
+          <a
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+        )}
+        
+        </div>
+      </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default AIWritingAssistant;
